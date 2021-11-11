9e04e0a342e6543ca555adae4b022312600fe206
Tamma <dev.mufti@gmail.com>
2021-11-11 21:26:24 +0700

    - add features - add NumberTrivia Page

---
+++ src/features/number-trivia/presentation/views/NumberTrivia.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Number Trivia</h1>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
+import Home from '@/features/home/presentation/views/Home.vue'
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,10 +10,8 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    
+    component: () => import('@/features/about/presentation/views/About.vue')
   }
 ]
 
