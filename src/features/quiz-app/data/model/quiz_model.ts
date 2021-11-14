0413bdf3bcef4530a8fa6072db609310ffd9edde
Rahmat Yani Hidayat <mortarboyz@gmail.com>
2021-11-14 11:23:14 +0700

    - modified model

--- src/features/quiz-app/data/model/quiz_model.ts
+++ src/features/quiz-app/data/model/quiz_model.ts
@@ -1,6 +1,9 @@
 // This model file contain representation 1:1 of the Quiz Response object from the API.
 // Response From : https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple
 interface QuizInterface {
+  category: string;
+  type: string;
+  difficulty: string;
   question: string;
   correct_answer: string;
   incorrect_answers: string[];
