099d790d6a8682c59f6857868253f04212168868
Rahmat Yani Hidayat <mortarboyz@gmail.com>
2022-01-11 22:14:09 +0700

    add link test

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/number-trivia">Number Trivia</router-link>
+    <router-link to="/quiz">Quiz</router-link>
   </div>
   <router-view/>
 </template>
